6049355b6f006463ac6250e4d5dbdb7a6013622c
Gijsbert <gijsbert.van.vliet@ing.nl>
2018-03-21 23:33:01 +0100

    create the parser from the google answer.

---
+++ src/main/scala/nl/ing/receiptLocations/GoogleAnswerJsonParser.scala
@@ -0,0 +1,42 @@
+package nl.ing.receiptLocations
+
+import scala.io.Source
+
+class GoogleAnswerJsonParser {
+
+  def parse(json: String): Schema = {
+    import spray.json._
+
+    val stream = getClass.getResourceAsStream("/Example.json")
+    val json = Source.fromInputStream(stream).mkString
+
+    val jsonObject = json.asJson.asJsObject
+    val jsonTextAnnotations = jsonObject.getFields("responses").head.asInstanceOf[JsArray].elements.head.asJsObject
+    val usefullJsonArray = jsonTextAnnotations.getFields("textAnnotations").head.asInstanceOf[JsArray].elements
+    val items = usefullJsonArray.map(jsonValue => {
+      val innerJsonObject = jsonValue.asJsObject()
+      val name = innerJsonObject.getFields("description").head.toString()
+      val vertices = innerJsonObject.getFields("boundingPoly").head.asJsObject.getFields("vertices").head.asInstanceOf[JsArray]
+      val corners = vertices.elements.map(element => {
+        val x = element.asJsObject.getFields("x").head.asInstanceOf[JsNumber].value.toInt
+        val y = element.asJsObject.getFields("y").head.asInstanceOf[JsNumber].value.toInt
+        nl.ing.receiptLocations.Vector(x, y)
+      })
+      val rectangle = Rectangle(corners(0), corners(1), corners(2), corners(3))
+      Item(name, rectangle)
+    })
+    Schema(items)
+  }
+ // {
+//    import spray.json._
+//    val jsonObject = json.asJson.asJsObject
+//    val usefullJsonArray: immutable.Seq[JsValue] = jsonObject.getFields("responses").head.asJsObject().getFields("textAnnotations")
+//    val items = usefullJsonArray.map(jsonValue => {
+//      val jsonObject = jsonValue.asJsObject()
+//      val name = jsonObject.getFields("description").head.toString()
+//      val vertices = jsonObject.getFields("boundingPoly").head.asJsObject.getFields("vertices")
+//    })
+//    Schema(items)
+//  }
+
+}
--- src/main/scala/nl/ing/receiptLocations/Schema.scala
+++ src/main/scala/nl/ing/receiptLocations/Schema.scala
@@ -2,16 +2,23 @@ package nl.ing.receiptLocations
 
 case class Vector(x: Double, y: Double)
 
-case class Line(slope: Double, hight: Double) {
-  def intersection(otherLine: Line): Option[Vector] = ???
+case class Line(slope: Double, height: Double) {
+  def intersection(otherLine: Line): Option[Vector] = {
+    if(otherLine.slope == slope) None
+    else {
+      val x = (otherLine.height - height)/(slope - otherLine.slope)
+      val y = slope * x + height
+      Some(Vector(x, y))
+    }
+  }
 }
 
-case class Square(topRight: Vector, topLeft: Vector, bottomRight: Vector, bottomLeft: Vector) {
+case class Rectangle(bottomLeft: Vector, bottomRight: Vector, upperRight: Vector, upperLeft: Vector) {
 
-  val middle: Vector = ???
+  lazy val middle: Vector = ???
 
-  def isOnSameHeight(otherSquare: Square): Boolean = ???
+  def isOnSameHeight(otherSquare: Rectangle): Boolean = ???
 }
-case class Item(name: String, coordinate: Square)
+case class Item(name: String, coordinate: Rectangle)
 
 case class Schema(item : Seq[Item])
