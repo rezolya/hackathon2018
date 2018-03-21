8fc34b20646218a1f1c7e7222454bf887c260747
Gijsbert <gijsbert.van.vliet@ing.nl>
2018-03-22 00:45:21 +0100

    Fill in methods.

--- src/main/scala/nl/ing/receiptLocations/Schema.scala
+++ src/main/scala/nl/ing/receiptLocations/Schema.scala
@@ -2,6 +2,37 @@ package nl.ing.receiptLocations
 
 case class Vector(x: Double, y: Double)
 
+object Line {
+
+  def getSlope(point1: Vector, point2: Vector): Option[Double] = {
+    if(point1.x == point2.x) None
+    else if(point1.x < point2.x) {
+      val xDifference = point2.x - point1.x
+      val yDifference = point2.y - point1.y
+      yDifference / xDifference
+    } else {
+      val xDifference = point1.x - point2.x
+      val yDifference = point1.y - point2.y
+      yDifference / xDifference
+    }
+  }
+
+  def getLineThroughPoint(point: Vector, slope: Double): Line = {
+    val height = point.y - (slope * point.x)
+    Line(slope, height)
+  }
+
+  def getLineThroughPoints(point1: Vector, point2: Vector): Option[Line] = {
+    getSlope(point1, point2) match {
+      case None =>
+        None
+      case Some(slope) =>
+        val height = point1.y - (slope * point1.x)
+        Some(Line(slope, height))
+    }
+  }
+}
+
 case class Line(slope: Double, height: Double) {
   def intersection(otherLine: Line): Option[Vector] = {
     if(otherLine.slope == slope) None
@@ -15,9 +46,41 @@ case class Line(slope: Double, height: Double) {
 
 case class Rectangle(bottomLeft: Vector, bottomRight: Vector, upperRight: Vector, upperLeft: Vector) {
 
-  lazy val middle: Vector = ???
+  import Line._
+  lazy val middle: Vector = getLineThroughPoints(bottomLeft, upperRight).get.intersection(getLineThroughPoints(bottomRight, upperLeft).get).get
 
-  def isOnSameHeight(otherSquare: Rectangle): Boolean = ???
+  def isToTheLeftOf (otherSquare: Rectangle): Boolean = {
+    Math.max(bottomRight.x, upperRight.x) <= Math.min(otherSquare.bottomLeft.x, otherSquare.upperLeft.x)
+  }
+
+  def isOnSameHeight(otherSquare: Rectangle): Boolean = {
+    (isToTheLeftOf(otherSquare), otherSquare.isToTheLeftOf(this)) match {
+      case (true, false) =>
+        val xUpper = otherSquare.upperLeft.x
+        val xBottom = otherSquare.bottomLeft.x
+        if(xUpper == xBottom) {
+          val lineThroughMiddle = getLineThroughPoint(middle, getSlope(bottomLeft, bottomRight).get)
+          val height = lineThroughMiddle.slope * xUpper + lineThroughMiddle.slope
+
+          otherSquare.bottomLeft.y <= height && height <= otherSquare.upperLeft.y
+        } else {
+
+          val lineThroughMiddle = getLineThroughPoint(middle, getSlope(bottomLeft, bottomRight).get)
+          val lineThroughLeftSideOfOtherRectangle = getLineThroughPoints(otherSquare.bottomLeft, otherSquare.upperLeft)
+          val intersection = lineThroughMiddle.intersection(lineThroughLeftSideOfOtherRectangle.get).get
+
+          if(xUpper < xBottom) {
+            xUpper < intersection.x && intersection.x < xBottom
+          } else {
+            xBottom < intersection.x && intersection.x < xUpper
+          }
+        }
+      case (false, true) =>
+        otherSquare.isOnSameHeight(this)
+      case (_, _) =>
+        false
+    }
+  }
 }
 case class Item(name: String, coordinate: Rectangle)
 
