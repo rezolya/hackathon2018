4c80e838ce78096a93d720a3c77aca0465356e6a
farhad sarabchi <farhadsarabchi@yahoo.com>
2018-03-21 20:15:48 +0100

    wip: add json reader schoma

---
+++ src/main/scala/nl/ing/Schema.scala
@@ -0,0 +1,11 @@
+package nl.ing
+
+
+case class vertices(topRight: Int, topLeft: Int, bottomRight: Int, bottomLeft: Int)
+case class item(name: String,coordinate: vertices )
+
+class Schema(item : Seq[item]) {
+
+
+
+}
