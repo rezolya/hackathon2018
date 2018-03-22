594e60a23e3cccd04ca6a70cd0f09d34a9774450
Fred Teunissen <Fred.Teunissen@ing.com>
2018-03-22 05:05:13 +0100

    Reading the AH products from th resources

--- src/test/scala/nl/ing/ClassifyFoodSpec.scala
+++ src/test/scala/nl/ing/ClassifyFoodSpec.scala
@@ -35,5 +35,10 @@ class ClassifyFoodSpec extends WordSpecLike with Matchers  {
       println(dif)
     }
 
+    "load CSV file" in {
+      val details: Seq[ItemDetails] = ClassifyFood.ahDB
+      details.length should be > 10
+      details.foreach(println)
+    }
   }
 }
